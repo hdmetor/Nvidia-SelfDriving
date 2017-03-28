be632ae07d7034c92f0a6f807f1fb04f8fe2fa31
Belli <hdmetor@gmail.com>
2017-03-27 19:19:53 -0700

    adding callback

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import load_data
 from model import NVIDA
+import time
 
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 
 print('Loading data...')
 train_x, train_y, test_x, test_y = load_data.return_data()
@@ -15,15 +16,21 @@ checkpointer = ModelCheckpoint(
     verbose=1,
     save_best_only=True
 )
+
+lr_plateau = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=0.000001)
+
 epochs = 100
 batch_size = 128
 
 print('Starting training')
-nvidia.fit(train_x, train_y,
+history = nvidia.fit(train_x, train_y,
     validation_data=(test_x, test_y),
     nb_epoch=epochs,
     batch_size=batch_size,
-    callbacks=[checkpointer]
+    callbacks=[checkpointer, lr_plateau]
 )
 
+with open('history_{}.json'.format(time.time()), 'wb') as fp:
+    # dict with np array as items are not serializable
+    json.dump(dict((k, np.array(v).tolist()) for k, v in history.history.items()), fp)
 print('Done')
